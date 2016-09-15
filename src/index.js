54ca9dc70a57e9a05f5f6af9ea3337e66c6ac387
Igor Suvorov <suvorov.igor.90@gmail.com>
2016-09-15 12:17:11 +0400

    eventemitter2, socket.io, socketio-wildcard

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@
 import http from 'http';
 import express from 'express';
 import ws from 'ws';
+import socketIO from 'socket.io';
+import EventEmitter from 'eventemitter2'
 
 import websocketUrl from './websocket-url';
 import addWsMethod from './add-ws-method';
@@ -37,39 +39,95 @@ export function expressWs(app, httpServer, options = {}) {
     addWsMethod(express.Router);
   }
 
-  const wsServer = new ws.Server({ server });
+  let socketServer = null
+  if(options.isSocketIO){
+    socketServer = socketIO.listen(server)
+    socketServer.use(require('socketio-wildcard')());
+    socketServer.on('connection', (socket) => {
+      socket.on('*', function(pack){
+        if(pack.data[0][0] == '/'){
+          const request = socket.request;
 
-  wsServer.on('connection', (socket) => {
-    const request = socket.upgradeReq;
+          request._ws = socket;
+          request.ws = new EventEmitter({wildcard: true});
+          request.ws.on('*', function(...args) {
+            args.unshift( pack.data[0], this.event)
+            request._ws.emit(...args)
+          })
+          request.ws.data = pack.data
+          request.ws.event = pack.data[1]
+          request.ws.body = pack.data[2]
+          request.url = websocketUrl(pack.data[0])
 
-    request.ws = socket;
-    request.wsHandled = false;
+          request.wsHandled = false;
 
-    /* By setting this fake `.url` on the request, we ensure that it will end up in the fake
-     * `.get` handler that we defined above - where the wrapper will then unpack the `.ws`
-     * property, indicate that the WebSocket has been handled, and call the actual handler. */
-    request.url = websocketUrl(request.url);
+          request.wsData = pack.data
+          request.event = pack.data[1]
+          request.body = pack.data[2]
+          const dummyResponse = new http.ServerResponse(request);
 
-    const dummyResponse = new http.ServerResponse(request);
+          dummyResponse.writeHead = function writeHead(statusCode) {
+            // if (statusCode > 200) {
+            //   /* Something in the middleware chain signalled an error. */
+            //   socket.close();
+            // }
+          };
 
-    dummyResponse.writeHead = function writeHead(statusCode) {
-      if (statusCode > 200) {
-        /* Something in the middleware chain signalled an error. */
-        socket.close();
-      }
-    };
+          app.handle(request, dummyResponse, () => {
+            // if (!request.wsHandled) {
+            //   /* There was no matching WebSocket-specific route for this request. We'll close
+            //    * the connection, as no endpoint was able to handle the request anyway... */
+            //   socket.close();
+            // }
+          });
 
-    app.handle(request, dummyResponse, () => {
-      if (!request.wsHandled) {
-        /* There was no matching WebSocket-specific route for this request. We'll close
-         * the connection, as no endpoint was able to handle the request anyway... */
-        socket.close();
-      }
+        }
+      })
+      //   require('fs').writeFileSync(__dirname + '/1.json', JSON.stringify(client))
+      //   //, client.socket
+      // // client.on('hi', this.log.error)
+      // client.on('join', function(data) {
+      //     console.log(data);
+      //     client.emit('messages', 'Hello from server');
+      //     client.emit('news', 'Hello from server');
+      // });
+    })
+  } else {
+    const wsServer = new ws.Server({ server });
+    wsServer.on('connection', (socket) => {
+      const request = socket.upgradeReq;
+
+      request.ws = socket;
+      request.wsHandled = false;
+
+      /* By setting this fake `.url` on the request, we ensure that it will end up in the fake
+       * `.get` handler that we defined above - where the wrapper will then unpack the `.ws`
+       * property, indicate that the WebSocket has been handled, and call the actual handler. */
+      request.url = websocketUrl(request.url);
+
+      const dummyResponse = new http.ServerResponse(request);
+
+      dummyResponse.writeHead = function writeHead(statusCode) {
+        if (statusCode > 200) {
+          /* Something in the middleware chain signalled an error. */
+          socket.close();
+        }
+      };
+
+      app.handle(request, dummyResponse, () => {
+        if (!request.wsHandled) {
+          /* There was no matching WebSocket-specific route for this request. We'll close
+           * the connection, as no endpoint was able to handle the request anyway... */
+          socket.close();
+        }
+      });
     });
-  });
+
+  }
 
   return {
     app,
+    socketIO: socketServer,
     getWss: function getWss() {
       return wsServer;
     },
